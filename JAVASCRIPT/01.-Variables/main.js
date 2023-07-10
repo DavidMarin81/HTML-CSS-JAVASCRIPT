e6f42cf20f205adc9bfb33829c3582d53fc6d888
David <davidmaringomez81@gmail.com>
2023-07-10 12:57:40 +0200

    Primer commit

---
+++ JAVASCRIPT/01.-Variables/main.js
@@ -0,0 +1,16 @@
+var nombre = "David";
+
+//Manda un mensaje de alerta en la página web
+alert("Bienvenido " + nombre);
+
+
+//Con prompt, se piden datos al usuario
+var nombre2;
+
+//El resultado se recoge en una variable
+nombre2 = prompt("Ingresa tu nombre");
+
+alert("Bienvenido " + nombre2);
+
+//Para escribir la variable en la página web
+document.write(nombre2);
