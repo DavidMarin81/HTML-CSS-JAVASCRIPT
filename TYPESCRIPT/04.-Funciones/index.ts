bbbb0bf458a5d5585544cd15cc5d08529e500e3f
David <davidmaringomez81@gmail.com>
2023-07-17 17:39:05 +0200

    Funciones

---
+++ TYPESCRIPT/04.-Funciones/index.ts
@@ -0,0 +1,19 @@
+//Funciones
+
+    function sumar(m : number, n : number) : number{
+        let suma = m + n;
+        console.log(`${m} + ${n} = ${suma}`);
+        return suma;
+    }
+
+    sumar(3, 4);
+
+//Funcion con argumento de tipo funcion
+    let multiplicar : (m : number, n : number) => number;
+    multiplicar = (m, n) => m * n;
+
+    let dividir : (a : number, b : number, callback : (r : number) => void) => void;
+    dividir = (a, b, callback) => callback(a * b);
+    dividir(4, 5, function(result) {
+        console.log(`Resultado: ${result}`);
+    })
